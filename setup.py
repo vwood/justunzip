4a1f3a08357933d2350b1ed9deec216965769f55
vwood <vwood@vwood.org>
2021-12-27 21:46:49 +1300

    Add more details to setup.py

--- setup.py
+++ setup.py
@@ -7,26 +7,31 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="justunzip",
-    version="0.0.3",
-    license='MIT',
+    version="0.0.4",
+    license="MIT",
     author="vwood",
     author_email="vwood@vwood.org",
     description="unzip truncated or corrupt zip files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    entry_points = {
-        'console_scripts': ['justunzip=justunzip.justunzip:main'],
-        },
+    entry_points={
+        "console_scripts": ["justunzip=justunzip.justunzip:main"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: System :: Archiving :: Compression",
+        "Topic :: Utilities",
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=[
-        'click>=7.1',
+        "click>=7.1",
     ],
     python_requires=">=3.6",
-    download_url="https://github.com/vwood/justunzip/archive/refs/tags/v0.0.3.tar.gz"
+    download_url="https://github.com/vwood/justunzip/archive/refs/tags/v0.0.4.tar.gz",
+    url="https://github.com/vwood/justunzip",
+    keywords="zip truncated corrupt zipfile archive unzip",
 )
